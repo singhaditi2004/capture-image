f617fd1f0cee2878e6a6a0296bf291ecd06e2d18
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-08-29 10:30:57 +0530

    getting image from gallery

--- app/src/main/java/com/example/getimage/MainActivity.java
+++ app/src/main/java/com/example/getimage/MainActivity.java
@@ -17,9 +17,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    AppCompatButton saveBtn;
+    AppCompatButton saveBtn,gGallery;
     ImageView img;
     private final int RESULT_CODE=1;
+    private final int GALLERY_REQUEST=2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +33,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(i,RESULT_CODE);
+            }
+        });
+        gGallery=findViewById(R.id.gallery);
+        gGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 Intent iG=new Intent(Intent.ACTION_PICK);
+                 iG.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                 startActivityForResult(iG,GALLERY_REQUEST);
             }
         });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -48,6 +58,9 @@ public class MainActivity extends AppCompatActivity {
             if(requestCode==RESULT_CODE){
                 Bitmap bit=(Bitmap)data.getExtras().get("data");
                 img.setImageBitmap(bit);
+            }
+            if(requestCode==GALLERY_REQUEST){
+                img.setImageURI(data.getData());
             }
         }
     }
